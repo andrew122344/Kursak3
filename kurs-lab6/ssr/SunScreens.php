b73ccd53ba7d379d9b27c06a02c2abce78882b7f
Andrew <andrii.manilets-ki231@nung.edu.ua>
2025-12-19 23:42:36 +0200

    laba 9 (kursak)

--- kurs-lab6/ssr/SunScreens.php
+++ kurs-lab6/ssr/SunScreens.php
@@ -65,7 +65,8 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     <head>
         <meta charset="utf-8"/>
         <title>Cонцезахисні засоби</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">   
+        <link rel="stylesheet" href="./style/custom.css"> 
     </head>
     <body>
         <div class="container">
